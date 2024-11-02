c887eb6c0c3b7de286eae18881f6b8383ac03903
cosmo1avo <avo.green.boi@gmail.com>
2024-11-02 19:25:31 +0200

    Am inceput fundamentalele jocului

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 Introducere:
 Scopul jocului este acela de a ajunge la 11 puncte primul. Exista cazuri speciale in care daca sunt scorurile 6-0 sau 9-1 meciul se termina in favoarea celui cu punctajul mai mare. In acelasi timp daca ambele tabere ajung la 10 castigatorul va fii primul care castiga 2 puncte consecutive.
 
-Tenisul de masa este un joc individual jucat intre doi adverasi P1 si P2. Masa de joc va fi reprezentata de un vector masa[9]. 4 va fii mijlocul mesei si cu cat numarul este mai aproape de o extremitate, cu atat este mai aproape de o margine a mesei (stanga sau dreapta).
+Tenisul de masa este un joc individual jucat intre doi adversari P1 si P2. Masa de joc va fi reprezentata de un vector masa[9]. 4 va fii mijlocul mesei si cu cat numarul este mai aproape de o extremitate, cu atat este mai aproape de o margine a mesei (stanga sau dreapta).
 
 Fiecare jucator are 5 serve, iar in cazul in care se ajunge la avantaje (ambii jucatori au ajuns la 10) fiecare jucator va avea cate o serva.
 
-Pe masa de ping pong exista un fileu. Daca un jucator alege sa atace cu putere prea mica, acesta va lovi fileul si va pierde punctul. Daca, doar in momentul servei, mingea doar atinge fileul mingea va fii repetata. De asemenea daca un jucator alege sa atace cu o putere prea mare acesta va da in afara terenului si va pierde punctul.
+Pe masa de ping pong exista un fileu. Daca un jucator alege sa atace cu putere prea mica, acesta va lovi fileul si va pierde punctul. Daca, doar in momentul servei, mingea doar atinge fileul, mingea va fii repetata. De asemenea daca un jucator alege sa atace cu o putere prea mare acesta va da in afara terenului si va pierde punctul.
 
 Mod de joc:
 
--- main.cpp
+++ main.cpp
@@ -4,19 +4,71 @@
 #include <SFML/Graphics.hpp>
 #include <Helper.h>
 
+class animation
+{
+public:
+   animation(sf::Texture* texture, sf::Vector2u imageCount, float switchtime);
+   ~animation();
+
+public:
+   sf::IntRect uvrect;
+
+private:
+   sf::Vector2u imagecount;
+   sf::Vector2u currentimage;
+
+   float totaltime;
+   float switchtime;
+};
+
+
+class racket
+{
+public:
+   std::string name;
+
+   std::string returnname()
+   {
+      return name;
+   }
+
+};
+
 int main() {
    sf::RenderWindow window(sf::VideoMode(800, 800), "Ultimate Ping-Pong");
+   sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+   player.setPosition(400.0f, 400.0f);
+   sf::Texture playertexture;
+   playertexture.loadFromFile("E:/fac/pooproject/UltimatePingPong/textures/test.png");
+   player.setTexture(&playertexture);
+   sf::Vector2u texturesize = playertexture.getSize();
+   texturesize.x /= 2;
+   player.setTextureRect(sf::IntRect(texturesize.x , 0, texturesize.x, texturesize.y));
    window.setFramerateLimit(60);
    while (window.isOpen())
    {
-      sf::Event event{};
-      while (window.pollEvent(event))
+      sf::Event evnt{};
+      while (window.pollEvent(evnt))
       {
-         if (event.type == sf::Event::Closed)
-            window.close();
+         switch (evnt.type)
+         {
+            case sf::Event::Closed:
+               window.close();
+               break;
+         }
       }
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+         player.move(-0.5f, 0.0f);
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+         player.move(0.5f, 0.0f);
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+         player.move(0.0f, -0.5f);
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+         player.move(0.0f, 0.5f);
       window.clear(sf::Color::Black);
+      window.draw(player);
       window.display();
    }
+
    return 0;
 }
