f5855d4b35a848b7f0f2562a10ff8c996fdd147a
cosmo1avo <avo.green.boi@gmail.com>
2024-11-03 14:53:20 +0200

    am creat animatie pentru miscare si pentru idle

--- main.cpp
+++ main.cpp
@@ -7,8 +7,40 @@
 class animation
 {
 public:
-   animation(sf::Texture* texture, sf::Vector2u imageCount, float switchtime);
-   ~animation();
+   animation(sf::Texture* texture, sf::Vector2u imagecount, float switchtime)
+   {
+      this->imagecount = imagecount;
+      this->switchtime = switchtime;
+      totaltime = 0.0f;
+      currentimage.x=0;
+      uvrect.width=texture->getSize().x / float(imagecount.x);
+      uvrect.height=texture->getSize().y / float(imagecount.y);
+   }
+   void update(int row, float deltatime, bool faceright)
+   {
+      currentimage.y = row;
+      totaltime += deltatime;
+      if (totaltime >= switchtime)
+      {
+         totaltime -= switchtime;
+         currentimage.x++;
+         if (currentimage.x >= imagecount.x)
+         {
+            currentimage.x=0;
+         }
+      }
+      uvrect.top=currentimage.y * uvrect.height;
+      if(faceright)
+      {
+         uvrect.left=currentimage.x * uvrect.width;
+         uvrect.width = abs(uvrect.width);
+      }
+      else
+      {
+         uvrect.left=(currentimage.x + 1) * abs(uvrect.width);
+         uvrect.width = -abs(uvrect.width);
+      }
+   }
 
 public:
    sf::IntRect uvrect;
@@ -22,31 +54,68 @@ private:
 };
 
 
-class racket
+class player
 {
 public:
-   std::string name;
-
-   std::string returnname()
+   player(sf::Texture* texture, sf::Vector2u imagecount, float switchtime,float speed):
+      animation(texture, imagecount, switchtime)
    {
-      return name;
+      this->speed = speed;
+      row = 0;
+      faceright = true;
+
+      body.setSize(sf::Vector2f(100.0f, 100.0f));
+      body.setPosition(400.0f, 400.0f);
+      body.setTexture(texture);
    }
+   void update(float deltatime)
+   {
+      sf::Vector2f movement(0.0f, 0.0f);
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+         movement.x -= speed * deltatime;
+      if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+         movement.x += speed * deltatime;
+      if(movement.x == 0.0f)
+         row = 0;
+      else
+      {
+         row =1;
+         if(movement.x>0.0f)
+            faceright = true;
+         else
+            faceright = false;
+      }
+      animation.update(row,deltatime,faceright);
+      body.setTextureRect(animation.uvrect);
+      body.move(movement);
+   }
+      void draw(sf::RenderWindow& window)
+   {
+      window.draw(body);
+   }
+
+private:
+   sf::RectangleShape body;
+   animation animation;
+   unsigned int row;
+   float speed;
+   bool faceright;
 
 };
 
 int main() {
    sf::RenderWindow window(sf::VideoMode(800, 800), "Ultimate Ping-Pong");
-   sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
-   player.setPosition(400.0f, 400.0f);
    sf::Texture playertexture;
    playertexture.loadFromFile("E:/fac/pooproject/UltimatePingPong/textures/test.png");
-   player.setTexture(&playertexture);
-   sf::Vector2u texturesize = playertexture.getSize();
-   texturesize.x /= 2;
-   player.setTextureRect(sf::IntRect(texturesize.x , 0, texturesize.x, texturesize.y));
+
+   player player(&playertexture, sf::Vector2u(2,2), 0.5, 100.0f);
+   float deltatime=0.0f;
+   sf::Clock clock;
+
    window.setFramerateLimit(60);
    while (window.isOpen())
    {
+      deltatime = clock.restart().asSeconds();
       sf::Event evnt{};
       while (window.pollEvent(evnt))
       {
@@ -57,16 +126,9 @@ int main() {
                break;
          }
       }
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-         player.move(-0.5f, 0.0f);
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-         player.move(0.5f, 0.0f);
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-         player.move(0.0f, -0.5f);
-      if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-         player.move(0.0f, 0.5f);
+      player.update(deltatime);
       window.clear(sf::Color::Black);
-      window.draw(player);
+      player.draw(window);
       window.display();
    }
 
