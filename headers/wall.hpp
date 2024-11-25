520de40395064acb9aae01fb8de0b35404ba2ff9
cosmo1avo <avo.green.boi@gmail.com>
2024-11-25 21:02:11 +0200

    constr de copiere

--- headers/wall.hpp
+++ headers/wall.hpp
@@ -11,16 +11,23 @@ public:
         body.setTexture(texture);
         body.setPosition(position);
     }
+    wall(const wall& other)
+    {
+        body.setSize(other.body.getSize());
+        body.setOrigin(other.body.getOrigin());
+        body.setTexture(other.body.getTexture());
+        body.setPosition(other.body.getPosition());
+    }
 
     void draw(sf::RenderWindow& window)
     {
         window.draw(body);
     }
+
     collider getcollider()
     {
         return collider(body);
     }
-
 
 private:
     sf::RectangleShape body;
